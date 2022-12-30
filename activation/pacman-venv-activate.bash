783e1cd587fe61daa7e1e1a260adbcd9bf0442ba
Joey H <joeyholtzman0507@gmail.com>
2022-12-30 12:56:31 -0600

    Initial commit
    1. Add basic creation of the virtual environment
    2. Add activation script for Bash
    3. Add the needed "shims" for the supported package managers

---
+++ activation/pacman-venv-activate.bash
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+# This file is a modified version from Python's virtualenv
+# https://github.com/pypa/virtualenv/blob/main/src/virtualenv/activation/bash/activate.sh
+
+if [[ "${BASH_SOURCE-}" == "$0" ]]; then
+    echo "This script must be sourced. Use source ${0}"
+    exit 1
+fi
+
+exit() {
+	# Reset all variables to their old values
+    PATH="${_PACMAN_VENV_OLD_PATH}"
+    PS1="${_PACMAN_VENV_OLD_PS1}"
+    LD_LIBRARY_PATH="${_PACMAN_VENV_OLD_LD_LIBRARY_PATH}"
+    PACMAN="${_PACMAN_VENV_OLD_PACMAN}"
+
+	# Unset all the temporary variables/functions
+    unset _PACMAN_VENV_OLD_PATH
+    unset _PACMAN_VENV_OLD_PS1
+    unset _PACMAN_VENV_OLD_LD_LIBRARY_PATH
+    unset _PACMAN_VENV_OLD_PACMAN
+    unset _PACMAN_VENV
+    unset -f exit
+
+	# Reset the sudo alias if it was set before.
+	# Otherwise, unset it
+    if [[ ${_PACMAN_VENV_OLD_SUDO} ]]; then
+		eval "${_PACMAN_VENV_OLD_SUDO}"
+    else
+		unalias sudo
+    fi
+
+    unalias pacman yay
+
+	# Forget past commands because the $PATH changed
+    hash -r
+}
+
+# Alias for exit
+deactivate() {
+    unset -f deactivate
+    exit
+}
+
+# Store all the old values of the variables
+_PACMAN_VENV_OLD_PATH="${PATH}"
+_PACMAN_VENV_OLD_PS1="${PS1}"
+_PACMAN_VENV_OLD_LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+_PACMAN_VENV_OLD_PACMAN="${PACMAN}"
+_PACMAN_VENV_OLD_SUDO="$(alias sudo 2>/dev/null)"
+
+# Name of the virtual environment
+_PACMAN_VENV="$(realpath "${BASH_SOURCE[0]}")"
+_PACMAN_VENV="$(dirname "$(dirname "$(dirname "${_PACMAN_VENV}")")")"
+export _PACMAN_VENV
+
+# Set all the needed values for the virtual environment
+export LD_LIBRARY_PATH="${_PACMAN_VENV}/lib:${LD_LIBRARY_PATH}"
+export PATH="${_PACMAN_VENV}/bin:${PATH}"
+PS1="[$(basename "${_PACMAN_VENV}")] ${PS1}"
+export PS1
+
+# Makepkg uses this variable to execute its Pacman command.
+# However, we need to override the default to use the --root flag,
+# so point makepkg to our pacman command.
+export PACMAN="${_PACMAN_VENV}/.pacman-venv-pacman"
+
+# Preserve env variables and allow sudo to be run with aliases
+alias sudo="sudo -E "
+
+# Point the package managers to Pacman Venv's command (include --root flag)
+alias pacman='${PACMAN}'
+alias yay='${_PACMAN_VENV}/.pacman-venv-yay'
+
+# Forget past commands because the $PATH changed
+hash -r
